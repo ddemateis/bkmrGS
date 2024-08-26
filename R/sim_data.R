cb9073d42892462374d4d7e53b27540b7864dc16
Danielle Demateis <Danielle.Demateis@colostate.edu>
2024-08-25 20:20:39 -0600

    added data set for simulation, data_standardized

---
+++ R/sim_data.R
@@ -0,0 +1,10 @@
+#' Data set for simulation
+#'
+#' @description Data set for simulation
+#'
+#' @format A data frame of 1000 rows and 37 columns
+#'
+#' @source Data source??
+#'
+#' @examples data(data_standardized) # lazy load
+"data_standardized"
