8ab083abf51b467d6df28f68ebf5968836a6ce69
Danielle Demateis <Danielle.Demateis@colostate.edu>
2024-11-25 10:31:37 -0700

    small changes and incorporating modification to interaction effect functions

--- R/ComputeMixtureSummaries.R
+++ R/ComputeMixtureSummaries.R
@@ -1,3 +1,4 @@
+#used in non-interactive effect functions (overall, single-exposure)
 riskSummary.approx <- function(point1, point2, preds.fun, ...) {
   cc <- c(-1, 1)
   newz <- rbind(point1, point2)
@@ -6,6 +7,8 @@ riskSummary.approx <- function(point1, point2, preds.fun, ...) {
   diff.sd <- drop(sqrt(cc %*% preds$postvar %*% cc)) #V[AX] = A * V[X] *A'
   c(est = diff, sd = diff.sd)
 }
+
+#not used
 riskSummary.samp <- function(point1, point2, preds.fun, ...) {
   cc <- c(-1, 1)
   newz <- rbind(point1, point2)
@@ -13,15 +16,25 @@ riskSummary.samp <- function(point1, point2, preds.fun, ...) {
   diff.preds <- drop(preds %*% cc)
   c(est = mean(diff.preds), sd = sd(diff.preds))
 }
+
+#used in interaction effect functions
 interactionSummary.approx <- function(newz.q1, newz.q2, modnew.1, modnew.2, preds.fun, ...) {
-  cc <- c(-1*c(-1, 1), c(-1, 1))
   newz <- rbind(newz.q1, newz.q2)
-  modnew <- rbind(modnew.1, modnew.2)
+  modnew <- c(modnew.1, modnew.2)
   preds <- preds.fun(newz, modnew, ...)
-  int <- drop(cc %*% preds$postmean) #E[AX] = A*E[X]
-  int.se <- drop(sqrt(cc %*% preds$postvar %*% cc)) #V[AX] = A * V[X] *A'
+  cc <- c(-1*c(-1, 1), c(-1, 1))
+  if("matrix" %in% class(preds)){
+    post_samp <- preds %*% matrix(cc, ncol=1)
+    int <- drop(mean(post_samp))
+    int.se <- drop(sd(post_samp))
+  }else{
+    int <- drop(cc %*% preds$postmean) #E[AX] = A*E[X]
+    int.se <- drop(sqrt(cc %*% preds$postvar %*% cc)) #V[AX] = A * V[X] *A'
+  }
   c(est = int, sd = int.se)
 }
+
+#not used
 interactionSummary.samp <- function(newz.q1, newz.q2, preds.fun, ...) {
   cc <- c(-1*c(-1, 1), c(-1, 1))
   newz <- rbind(newz.q1, newz.q2)
@@ -225,6 +238,10 @@ SingVarRiskSummaries <- function(fit, y = NULL, Z = NULL, X = NULL,
       modifier <- fit$modifier 
   }
   
+  if(!is.null(m.fixed) & is.null(modifier)){
+    stop("Cannot specify modifier values for model without modification.")
+  }
+  
   if (is.null(z.names)) 
     z.names <- paste0("z", 1:ncol(Z))
   df <- dplyr::tibble()
@@ -254,20 +271,18 @@ SingVarRiskSummaries <- function(fit, y = NULL, Z = NULL, X = NULL,
   df
 }
 
-SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, modifier = NULL, qs.diff = c(0.25, 0.75), qs.fixed = c(0.25, 0.75), mod.diff = NULL, method = "approx", sel = NULL, ...) {
+#used in SingVarIntSummaries() below
+SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, 
+                              X = NULL, modifier = NULL, 
+                              qs.diff = c(0.25, 0.75), 
+                              qs.fixed = c(0.25, 0.75), mod.diff = NULL,
+                              method = "approx", sel = NULL, ...) {
   
   if (inherits(fit, "bkmrfit")) {
     if (is.null(y)) y <- fit$y
     if (is.null(Z)) Z <- fit$Z
     if (is.null(X)) X <- fit$X
     if (is.null(modifier)) modifier <- fit$modifier
-  }
-  
-  kernel.method <- fit$kernel.method
-  if(kernel.method == "one"){
-    kern_modifier <- NULL
-  }else if (kernel.method == "two"){
-    kern_modifier <- modifier
   }
   
   #second difference: h(z_1^{qs2}, z_2^{q2}, \dots, z_M^{q2}) - h(z_1^{qs1}, z_2^{q2}, \dots, z_M^{q2})
@@ -278,7 +293,7 @@ SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, mod
   newz.q1 <- rbind(point1, point2) 
   
   #tack on the modifier
-  if(!is.null(modifier)){
+  if(!is.null(modifier)){#need modnew even for two-kernel model, SamplePred handles it
     modnew.1 <- rep(mod.diff[2], 2)
   }else{
     modnew.1 <- rep(NULL, 2)
@@ -292,7 +307,7 @@ SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, mod
   newz.q2 <- rbind(point1, point2)
   
   #tack on the modifier
-  if(!is.null(modifier)){
+  if(!is.null(modifier)){ #need modnew even for two-kernel model, SamplePred handles it
     modnew.2 <-rep(mod.diff[1], 2)
   }else{
     modnew.2 <- rep(NULL, 2)
@@ -300,7 +315,29 @@ SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, mod
   
   if (method %in% c("approx", "exact")) {
     preds.fun <- function(znew, modnew){
-      ComputePostmeanHnew(fit = fit, y = y, Z = Z, X = X, Znew = znew, mod_new = modnew, sel = sel, method = method, modifier = kern_modifier)
+      if(is.null(modnew)){
+        ComputePostmeanHnew(fit = fit, 
+                            y = y, 
+                            Z = Z, 
+                            X = X, 
+                            Znew = znew, 
+                            sel = sel, 
+                            method = method)
+      }else{
+        if(method == "approx"){
+          warning("Approximate method not supported for modification. Using exact instead.")
+        }
+        SamplePred(fit = fit,
+                   y = y,
+                   Z = Z,
+                   X = X,
+                   modifier = modifier,
+                   Znew = znew,
+                   Xnew = matrix(0, nrow = nrow(znew), ncol = ncol(X)),
+                   mod_new = modnew,
+                   sel = sel)
+      }
+      
     }
     interactionSummary <- interactionSummary.approx
   } else {
@@ -311,14 +348,14 @@ SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, mod
 
 #' Single Variable Interaction Summaries
 #' 
-#' Compare the single-predictor health risks when all of the other predictors in Z are fixed to their a specific quantile to when all of the other predictors in Z are fixed to their a second specific quantile.
+#' Compare the single-predictor health risks when all of the other predictors in Z are fixed to their a specific quantile to when all of the other predictors in Z are fixed to their a second specific quantile. For interactive overall effect, set \code{mod.diff = c(0,1)}, and make \code{qs.diff} and \code{qs.fixed} the same vector. 
 #' @inheritParams kmbayes
 #' @inheritParams ExtractEsts
 #' @inheritParams SingVarRiskSummaries
 #' @inherit ComputePostmeanHnew details
 #' @param qs.diff vector indicating the two quantiles at which to compute the single-predictor risk summary
 #' @param qs.fixed vector indicating the two quantiles at which to fix all of the remaining exposures in \code{Z}
-#' @param mod.diff vector of two modifier values at which to compute the single-predictor risk summary (only for models with modification). Either \code{c(0,1)}, \code{c(0,0,)}, or \code{c(1,1)}.
+#' @param mod.diff vector of two modifier values at which to compute the single-predictor risk summary (only for models with modification.) For single exposure interactive effects in the same group, use \code{c(0,0)} or \code{c(1,1)}. For single-exposure interaction between groups, use \code{c(0,1)} and \code{qs.fixed} must be a vector of two of the same values. 
 #' @export
 #' 
 #' @return a data frame containing the (posterior mean) estimate and posterior standard deviation of the single-predictor risk measures
@@ -338,7 +375,12 @@ SingVarIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, X = NULL, mod
 #' fitkm <- kmbayes(y = y, Z = Z, X = X, iter = 100, verbose = FALSE, varsel = TRUE)
 #' 
 #' risks.int <- SingVarIntSummaries(fit = fitkm, method = "exact")
-SingVarIntSummaries <- function(fit, y = NULL, Z = NULL, X = NULL, modifier = NULL, which.z = 1:ncol(Z), qs.diff = c(0.25, 0.75), qs.fixed = c(0.25, 0.75), mod.diff = NULL, method = "approx", sel = NULL, z.names = colnames(Z), ...) {
+SingVarIntSummaries <- function(fit, y = NULL, Z = NULL, X = NULL,
+                                modifier = NULL, which.z = 1:ncol(Z), 
+                                qs.diff = c(0.25, 0.75), 
+                                qs.fixed = c(0.25, 0.75), 
+                                mod.diff = NULL, method = "approx", 
+                                sel = NULL, z.names = colnames(Z), ...) {
   
   if (inherits(fit, "bkmrfit")) {
     if (is.null(y)) y <- fit$y
@@ -347,14 +389,132 @@ SingVarIntSummaries <- function(fit, y = NULL, Z = NULL, X = NULL, modifier = NU
     if (is.null(modifier)) modifier <- fit$modifier
   }
   
+  if(!is.null(mod.diff) & is.null(modifier)){
+    stop("Cannot specify modifier values for model without modification.")
+  }
+  
   if(is.null(z.names)) z.names <- paste0("z", 1:ncol(Z))
   
+  #for each exposure, apply SingVarIntSummary()
   ints <- sapply(which.z, function(whichz)
-    SingVarIntSummary(whichz = whichz, fit = fit, Z = Z, X = X, y = y, modifier = modifier, qs.diff = qs.diff, qs.fixed = qs.fixed, mod.diff = mod.diff, method, sel = sel, ...)
+    SingVarIntSummary(whichz = whichz, 
+                      fit = fit, 
+                      Z = Z, 
+                      X = X, 
+                      y = y, 
+                      modifier = modifier, 
+                      qs.diff = qs.diff, 
+                      qs.fixed = qs.fixed, 
+                      mod.diff = mod.diff, 
+                      method = method, 
+                      sel = sel,
+                      ...)
   )
+  
   
   df <- dplyr::tibble(variable = factor(z.names[which.z], levels = z.names), est = ints["est", ], sd = ints["sd", ])
 }
 
+#used in OverallIntSummaries() below
+OverallIntSummary <- function(whichz = 1, fit, y = NULL, Z = NULL, 
+                              X = NULL, modifier = NULL, 
+                              qs = 0.25, q.fixed = 0.5,
+                              method = "approx", sel = NULL, ...) {
+  
+  if (inherits(fit, "bkmrfit")) {
+    if (is.null(y)) y <- fit$y
+    if (is.null(Z)) Z <- fit$Z
+    if (is.null(X)) X <- fit$X
+    if (is.null(modifier)) modifier <- fit$modifier
+  }
+  
+  #differences, regardless of modifier: h(z_1^{qs}, z_2^{qs}, \dots, z_M^{qs}) - h(z_1^{q}, z_2^{q}, \dots, z_M^{q})
+  point2 <- apply(Z, 2, quantile, q.fixed)
+  point1 <- apply(Z, 2, quantile, qs)
+  newz.q1 <- newz.q2 <- rbind(point1, point2) 
+  
+  #modifier
+  modnew.1 <- rep(1, 2)
+  modnew.2 <-rep(0, 2)
 
+  if (method %in% c("exact")) {
+    preds.fun <- function(znew, modnew){
+      SamplePred(fit = fit,
+                 y = y,
+                 Z = Z,
+                 X = X,
+                 modifier = modifier,
+                 Znew = znew,
+                 Xnew = matrix(0, nrow = nrow(znew), ncol = ncol(X)),
+                 mod_new = modnew,
+                 sel = sel)
+    }
+      
+    interactionSummary <- interactionSummary.approx
+  } else {
+    stop("method must be 'exact'")
+  }
+  interactionSummary(newz.q1, newz.q2, modnew.1, modnew.2, preds.fun, ...)
+}
 
+#' Overall Effect Interaction Summaries
+#' 
+#' Compare the overall effect difference between groups for models with modification 
+#' @inheritParams kmbayes
+#' @inheritParams ExtractEsts
+#' @inheritParams SingVarRiskSummaries
+#' @inherit ComputePostmeanHnew details
+#' @param qs vector of quantiles at which to calculate the overall risk summary 
+#' @param q.fixed a second quantile at which to compare the estimated \code{h} function
+#' @export
+#' 
+#' @return a data frame containing the (posterior mean) estimate and posterior standard deviation of the single-predictor risk measures
+#' 
+#' @examples
+#' ## First generate dataset
+#' set.seed(111)
+#' dat <- SimData(n = 50, M = 4)
+#' y <- dat$y
+#' Z <- dat$Z
+#' X <- dat$X
+#' 
+#' ## Fit model with component-wise variable selection
+#' ## Using only 100 iterations to make example run quickly
+#' ## Typically should use a large number of iterations for inference
+#' set.seed(111)
+#' fitkm <- kmbayes(y = y, Z = Z, X = X, iter = 100, verbose = FALSE, varsel = TRUE)
+#' 
+#' risks.int <- SingVarIntSummaries(fit = fitkm, method = "exact")
+OverallIntSummaries <- function(fit, y = NULL, Z = NULL, X = NULL,
+                                modifier = NULL, which.z = 1:ncol(Z), 
+                                qs = seq(0.25, 0.75,0.05), 
+                                q.fixed = 0.5, method = "approx", 
+                                sel = NULL, z.names = colnames(Z), ...) {
+  
+  if (inherits(fit, "bkmrfit")) {
+    if (is.null(y)) y <- fit$y
+    if (is.null(Z)) Z <- fit$Z
+    if (is.null(X)) X <- fit$X
+    if (is.null(modifier)) modifier <- fit$modifier
+  }
+  
+  if(is.null(modifier)){
+    stop("Cannot compute overall interactive effect between groups for model without modification.")
+  }
+  
+  if(is.null(z.names)) z.names <- paste0("z", 1:ncol(Z))
+  
+  #for each quantile, apply OverallIntSummary()
+  risks.overall <- t(sapply(qs, function(q_s)
+    OverallIntSummary(fit = fit, 
+                      Z = Z, 
+                      X = X, 
+                      y = y, 
+                      modifier = modifier, 
+                      qs = q_s, 
+                      q.fixed = q.fixed, 
+                      method = method, 
+                      sel = sel,
+                      ...)))
+  risks.overall <- data.frame(quantile = qs, risks.overall)
+}
