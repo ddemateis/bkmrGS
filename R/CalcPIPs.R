cd6b0eab6e9f1392f8ecebe77284a7b0fea05278
Danielle Demateis <Danielle.Demateis@colostate.edu>
2025-06-30 14:49:50 -0400

    examples update

--- R/CalcPIPs.R
+++ R/CalcPIPs.R
@@ -74,7 +74,7 @@ CalcPIPs <- function(fit, sel = NULL) {
 #' @param z.names optional argument providing the names of the variables included in the \code{h} function.
 #'
 #' @return a data frame with the variable-specific PIPs for BKMR fit with component-wise variable selection, and with the group-specific and conditional (within-group) PIPs for BKMR fit with hierarchical variable selection.
-#' @details For guided examples, go to \url{https://jenfb.github.io/bkmr/overview.html}
+#' @details For guided examples, see vignette(bkmrGSOverview)
 #' @export
 #' 
 #' @examples
