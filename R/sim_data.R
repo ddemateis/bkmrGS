d9d08b4303a9b85055d51c383c51d50422f1ce8e
Danielle Demateis <Danielle.Demateis@colostate.edu>
2025-06-30 14:55:24 -0400

    change to data source

--- R/sim_data.R
+++ R/sim_data.R
@@ -4,7 +4,7 @@
 #'
 #' @format A list of response (y), 3 exposures (Z), binary modifier (modifier), and 14 covariates (X)
 #'
-#' @source Liu et al. 2021
+#' @source Liu, J. Z., Deng, W., Lee, J., Lin, P. i D., Valeri, L., Christiani, D. C., … Coull, B. A. (2021). A Cross-Validated Ensemble Approach to Robust Hypothesis Testing of Continuous Nonlinear Interactions: Application to Nutrition-Environment Studies. Journal of the American Statistical Association, 117(538), 561–573. https://doi.org/10.1080/01621459.2021.1962889
 #'
 #' @examples data(ex_data) # lazy load
 "ex_data"
